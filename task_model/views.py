0b9c8056534e4ca3f133e1c407981dd65cc22e2a
MahadiMunna <mahadimunna.official@gmail.com>
2024-01-01 23:53:46 +0600

    delete and edit done

--- task_model/views.py
+++ task_model/views.py
@@ -12,3 +12,17 @@ def add_task(request):
     else:
         task_form = TaskModelForm()
     return render(request, 'add_task.html', {'form': task_form})
+
+def edit_task(request, id):
+    task = TaskModel.objects.get(id=id)
+    task_form = TaskModelForm(instance=task)
+    if request.method == 'POST':
+        task_form = TaskModelForm(request.POST, instance=task)
+        if task_form.is_valid():
+            task_form.save()
+            return redirect('home')
+    return render(request, 'edit_task.html', {'form': task_form})
+
+def delete_task(request, id):
+    TaskModel.objects.get(id=id).delete()
+    return redirect('home')
